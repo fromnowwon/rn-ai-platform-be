28840ddbb52e5365fa55c138ebd577b7b17c4aa9
Chaewon Jeong <fromnowwon@gmail.com>
2025-08-20 18:57:06 +0900

    first commit

---
+++ src/main/java/com/fromnowwon/rnaiplatform/RnAiPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.fromnowwon.rnaiplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnAiPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RnAiPlatformApplication.class, args);
+	}
+
+}
